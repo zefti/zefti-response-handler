7f6775359fa4f27316b21f5e8309af06684d7a16
Ryan Ogle <ryan@zefti.com>
2015-06-25 12:36:45 +0700

    stuff

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-module.exports = function(payload, cb) {
-  cb(null, payload);
-}
+var config = require('zefti-config');
+var utils = require('zefti-utils')({});
+module.exports = function(options) {
+  var responseFields = null;
+  var responseFieldsOption = options.responseFields;
+
+  if (typeof responseFieldsOption === 'string') {
+    responseFields = config.response[responseFieldsOption];
+  } else {
+    responseFields = responseFieldsOption;
+  }
+  if (!responseFields || utils.type(responseFields) !== 'array') throw new Error('responseFields is not formed well');
+
+  function responseHandler(payload, cb) {
+    var response = {};
+    responseFields.forEach(function (field) {
+      if (payload[field]) {
+        response[field] = payload[field];
+      }
+    });
+    payload.res.send(response);
+  }
+
+  return responseHandler;
+
+};
